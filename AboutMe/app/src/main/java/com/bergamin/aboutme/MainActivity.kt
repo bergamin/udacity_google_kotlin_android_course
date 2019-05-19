e023f0a914bd811f9d1415c769c13c655141eabe
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2019-05-19 17:09:45 -0300

    Lesson 2, exercises 11 to 15 - Added a way to add a nickname - Changed the font style

--- AboutMe/app/src/main/java/com/bergamin/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/bergamin/aboutme/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.bergamin.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var nicknameTextView: TextView
+    lateinit var doneButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.nickname_edit)
+        nicknameTextView = findViewById(R.id.nickname_text)
+        doneButton = findViewById(R.id.done_button)
+
+        doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
