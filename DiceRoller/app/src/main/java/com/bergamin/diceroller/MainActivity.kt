0ccc6c8f441bd290e13789f8c40cda6fe58fff93
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2019-05-18 23:26:58 -0300

    Lesson 1, exercises 21 to 29 - Added the dice images to the layout - Use of vectorDrawables - Refactor to androidx - Refactor diceImage to be a lateinit field

--- DiceRoller/app/src/main/java/com/bergamin/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/bergamin/diceroller/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.bergamin.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +18,20 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random.nextInt(6) + 1
-
-        resultText.text = randomInt.toString()
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
